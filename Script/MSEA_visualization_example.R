027128a422b2bef1be79811c62c288723d66f48f
Xingyue Wang <46041844+XINGYUE23@users.noreply.github.com>
2025-08-30 13:30:01 +0200

    Update MSEA_visualization_example.R

--- Script/MSEA_visualization_example.R
+++ Script/MSEA_visualization_example.R
@@ -3,8 +3,6 @@
 source("./Script/visualization.R") # functions for visualization
 
 testda <- rio::import( "./testda/testda_hubmet_0709.txt" )
-testda2 <- rio::import( "./testda/testda_public_0709.txt" )
- 
 
 hubres <- rio::import( "./Output/M2_MSEA/HUBMet_Disease_MSEA_forRvisu.txt" )
   
@@ -13,10 +11,4 @@ barplot_msea(hubres, "Disease")
 msea_heatmap(hubres,hmdb_list = testda$metID,hmdb_list_value = testda$fc,database = "Disease")
   
 msea_plot_ES_bar(database = "Disease",hubres$Term[1],hmdb_list = testda$metID,hmdb_list_value = testda$fc,res_msea = hubres)
- 
-
-pathview_kegg_smpdb_reactome("smpdb",pathway_ID = "SMP0000188", 
-                             hmdb_list = testda2$metID,
-                             hmdb_list_value = testda2$fc )
-
  
